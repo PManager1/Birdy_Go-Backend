c5c10ca5d3c98a3ebade23596daf3f459540543c
Jay <jpca999@gmail.com>
2025-08-30 12:48:54 -0400

    minor changes AND FRESH SEEM TO BE WORKING!!!

--- hello-go/main.go
+++ hello-go/main.go
@@ -89,7 +89,7 @@ func getUsersHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func getTripsHandler(w http.ResponseWriter, r *http.Request) {
-	collection := client.Database("testdb").Collection("trips") // Replace 'testdb' with your DB name
+	collection := client.Database("test").Collection("trips") // Replace 'testdb' with your DB name
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
